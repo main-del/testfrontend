c8f4862a8f21de4211a6cb25fa5be49d42d584c2
main-del <vajgleb03@gmail.com>
2023-12-09 00:42:26 +0300

    Update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://91.222.136.251:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
