eacff12a6f930faa31bf4f9da13a02681238c284
main-del <vajgleb03@gmail.com>
2023-12-10 20:02:25 +0300

    edit fetch ip

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://91.222.136.251:8000/web-data', {
+        fetch('http://91.222.136.251/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
